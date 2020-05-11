821d2230eecce7e0052538c0d42229b599041e8c
siddhusalvi <siddheshsalvi525@gmail.com>
2020-05-11 21:54:54 +0530

    ADDED : Program to print tweets

--- src/main/scala/TweetApp.scala
+++ src/main/scala/TweetApp.scala
@@ -1,7 +1,9 @@
 //Explanation : https://medium.com/analytics-vidhya/finding-the-most-popular-hashtags-on-twitter-using-spark-streaming-16c3fe09f734
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 import scala.io.Source
 
 object TweetApp {
@@ -11,7 +13,7 @@ object TweetApp {
     setupTwitter()
 
     // Creating a streaming context which will stream batches of data in every 1 second
-    val ssc = new StreamingContext("local[*]", "TweetApp", Seconds(10))
+    val ssc = new StreamingContext("local[*]", "TweetApp", Seconds(15))
 
     // Set log level to print errors only
     setupLogging()
@@ -19,30 +21,18 @@ object TweetApp {
     // Create a DStream from twitter
     val tweets = TwitterUtils.createStream(ssc, None)
 
-    // Extract text from tweets
-    val text = tweets.map(x => x.getText())
+    // Now extract the text of each status update into DStreams using map()
 
-    // Extract each word from text
-    val words = text.flatMap(x => x.split(" "))
+    val statuses = tweets.map(status => status.getText())
 
-    // Keep the words that start with a hashtag
-    val hashtags = words.filter(x => x.startsWith("#"))
+    statuses.foreachRDD(rdd => {
+      rdd.foreach(println(_))
+    })
 
-    val hashtags_values = hashtags.map(x => (x, 1))
-
-    // Count them over a 5 minute window every 1 second
-    val hashtags_count = hashtags_values.reduceByKeyAndWindow((x, y) => x + y, (x, y) => x - y, Seconds(300), Seconds(1))
-
-    // Sort the result by hashtag count in descending order
-    val results = hashtags_count.transform(x => x.sortBy(x => x._2, false))
-
-    // Print top 10 results
-    results.print
-
-    // Set up a checkpoint
     ssc.checkpoint("C://checkpoint")
     ssc.start()
     ssc.awaitTermination()
+
   }
 
   def setupLogging() = {
