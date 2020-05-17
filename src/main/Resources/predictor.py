abbfd475297ed335e663db651d3a0abaa0fc906b
siddhusalvi <siddheshsalvi525@gmail.com>
2020-05-17 17:11:07 +0530

    REFACTORED : Saved results in csv

--- src/main/Resources/predictor.py
+++ src/main/Resources/predictor.py
@@ -1,22 +1,12 @@
-# model_path = "C:\\Users\\Siddesh\\IdeaProjects\\TweeterApp\\src\\main\\Resources\\data.pkl"
-
 from nltk.corpus import stopwords
 import pickle
-# lemmatize like stem
 from nltk.stem import WordNetLemmatizer
-
-# To Wrap up the sklearn classifiers to be used in NLP for classifying
-from nltk.classify.scikitlearn import SklearnClassifier
-
 from sklearn.metrics import *
-from sklearn.naive_bayes import MultinomialNB
-
-import numpy as np
 import pandas as pd
 import sys
 
 a = sys.stdin.read()
-
+text= a
 if a == "":
     sys.exit()
 
@@ -25,8 +15,6 @@ for i in ab:
     if not i.isalpha() or i.startswith("@") or i.startswith("#"):
         ab.remove(i)
 a = " ".join(ab)
-
-
 
 class Preprocessor:
 
@@ -38,49 +26,29 @@ class Preprocessor:
                                               word in ['!', '.', ',']))])
         return doc.apply(preprop)
 
-
-# path of model in local system
-model_path = "C:\\Users\\Siddesh\\Downloads\\twiiter\\data.pkl"
-# for jenkins deployed model
-# model_path = "/home/ubuntu/tweeter_app/model.pkl"
-
+model_path = "C:\\Users\\Siddesh\\IdeaProjects\\TweeterApp\\src\\main\\Resources\\data.pkl"
 file = open(model_path, 'rb')
 pre_process = pickle.load(file)
 cv = pickle.load(file)
 mnb = pickle.load(file)
 file.close()
-
-
-# z = a.split("\n")
-# z = ["I wasn't aware of the two versions of the #EU do","Im Gesprch: Das Corona-Regime wird fallen "]
 df = pd.DataFrame()
 av= pd.Series(a)
-# for each in z:
-#     df = df.append([each])
 df = df.append(av,ignore_index=True)
-
-
-# new_df = pre_process.preprocessor(df[0])
-#
-# new_df.head()
-
 transfromed = cv.transform(df[0])
-
 transfromed
-
 pred = mnb.predict(transfromed)
-
 pred_str = []
-
+text = text.replace("\n", " ")
+text = text.replace(",", "")
+text += ","
 for i in range(0, len(pred)):
     if pred[i] == 0:
-        pred_str.append("negetive")
+        text += " negative"
     else:
-        pred_str.append("positive")
-
-
-df['pred'] = pred_str
-
-
-for each in df.values:
-    print(each)
+        text += " positive"
+text += "\n"
+filename = "C:\\Users\\Siddesh\\IdeaProjects\\TweeterApp\\src\\main\\Resources\\record.csv"
+file1 = open(filename,"a")#append mode
+file1.write(text)
+file1.close()
