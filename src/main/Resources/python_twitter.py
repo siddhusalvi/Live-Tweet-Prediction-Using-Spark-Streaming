573808f91ff9fe2ea063ac3ac50aa76a385c9978
siddhusalvi <siddheshsalvi525@gmail.com>
2020-05-19 17:13:48 +0530

    REFACTORED : Code cleanup

--- src/main/Resources/python_twitter.py
+++ src/main/Resources/python_twitter.py
@@ -1,82 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-import nltk
-# tokenizing in various ways
-from nltk.tokenize import word_tokenize
-# stopwwords collection
-from nltk.corpus import stopwords
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-
-import pickle
-# lemmatize like stem
-from nltk.stem import WordNetLemmatizer
- 
-# To Wrap up the sklearn classifiers to be used in NLP for classifying
-from nltk.classify.scikitlearn import SklearnClassifier
-
-from sklearn.metrics import *
-from sklearn.naive_bayes import MultinomialNB
-
-import numpy as np
-import pandas as pd
-import sys
-import os
-
-
-# In[ ]:
-
-
-#a = sys.stdin.read()
-#if a=="":
-#    sys.exit()
-
-# In[2]:
-
-
-#train_data, model_path = a.split()
-
 train_data = "C:\\Users\\Siddesh\\Downloads\\twiiter\\data.csv"
 model_path = "C:\\Users\\Siddesh\\Downloads\\twiiter\\data.pkl"
 
-# In[7]:
-
-
-# train_data = "Data/train.csv"
-# model_path = "model_pickle/classifier.pkl"
-
-
-# In[9]:
-
-
 train_df =pd.read_csv(train_data, encoding='utf-8')
-
-
-# In[10]:
-
-
 train_df.head()
-
-
-# In[11]:
-
-
 train_df=train_df[["label","tweet"]]
-
-
-# In[12]:
-
-
 train_df.head(3)
-
-
-# In[13]:
-
-
-# Removes stopwords and storing only storing alpabets and numbers and imp symbols also lemamtizing to decrease number of features
 class Preprocessor:
 
     def preprocessor(self,doc):
@@ -84,53 +12,12 @@ class Preprocessor:
         preprop = lambda x: ' '.join([lm.lemmatize(word) for word in x.split() if word not in stopwords.words('english') and not(word.isalpha() or word.startswith('@') or word.isnumeric() or (word in ['!','.',','])) ])
         return doc.apply(preprop)
 
-
-# In[14]:
-
-
 pre_process = Preprocessor()
-
-
-# In[15]:
-
-
 train_df= train_df.dropna()
-
-
-# In[16]:
-
-
 train_df.isnull().sum()
-
-
-# In[17]:
-
-
 train_df = train_df.drop_duplicates()
-
-
-# In[18]:
-
-
-train_df = train_df.drop(train_df[(train_df["tweet"]=="&quot")].index.values, axis=0) 
-# Delete all rows with label "Ireland"
-
-
-# In[19]:
-
-
+train_df = train_df.drop(train_df[(train_df["tweet"]=="&quot")].index.values, axis=0)
 train_df.shape[0]
-
-
-# In[15]:
-
-
-# storing preprocessor object
-
-
-# In[16]:
-
-
 class by_count_vectorizer:
     
     def check_result(self,feature,classifier):
@@ -165,17 +52,5 @@ class by_count_vectorizer:
         except Exception as e:
             print("Cannot return the path beacause ",e)
 
-#         feature = cv.transform(["The movie was pleasant"])
-#         self.check_result(feature,mnb)
-
-
-# In[17]:
-
-
 cv_classifier = by_count_vectorizer()
-
-
-# In[ ]:
-
-
 cv_classifier.classify_demo(train_df)
