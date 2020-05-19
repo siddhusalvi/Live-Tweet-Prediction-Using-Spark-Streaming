a724659e9ea7f42142cc06c43aa65b6a34ca3d1c
siddhusalvi <siddheshsalvi525@gmail.com>
2020-05-19 17:12:19 +0530

    REFACTORED : Variable names and cleanup

--- src/main/scala/TweetApp.scala
+++ src/main/scala/TweetApp.scala
@@ -1,47 +1,39 @@
 import java.nio.charset.StandardCharsets
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.rdd
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-
 object TweetApp {
   def main(args: Array[String]) {
-    try{
-    val spark = SparkSession.builder().master("local[*]").appName("TweetApp").getOrCreate()
-    val sc = spark.sparkContext
-    setupTwitter()
-
-    val ssc = new StreamingContext(sc, Seconds(5))
-    setupLogging()
-
-    val filter = Array("corona")
-    val tweets = TwitterUtils.createStream(ssc, None, filter)
-    val statuses = tweets.map(status => status.getText())
-    val command = "python src//main//Resources//predictor.py"
-
-    val tweet1 = tweets.map {
-      x => new String((x.getText).getBytes(StandardCharsets.US_ASCII), StandardCharsets.US_ASCII).replace("?", "")
-    }
-
-    tweet1.foreachRDD(
-      rdd => rdd.foreach(println(_))
-    )
-    tweet1.foreachRDD(
-
-      rdd => rdd.pipe(command).collect().foreach(println(_))
-    )
-
-    ssc.checkpoint("src//main//Resources//checkpoint")
-    ssc.start()
-    ssc.awaitTermination()
+    try {
+      val spark = SparkSession.builder().master("local[*]").appName("TweetApp").getOrCreate()
+      val sc = spark.sparkContext
+      setupTwitter()
+      val ssc = new StreamingContext(sc, Seconds(5))
+      setupLogging()
+      val filter = Array("corona")
+      val tweet_stream = TwitterUtils.createStream(ssc, None, filter)
+      val statuses = tweet_stream.map(status => status.getText())
+      val command = "python src//main//Resources//predictor.py"
+      val live_tweets = tweet_stream.map {
+        x => new String((x.getText).getBytes(StandardCharsets.US_ASCII), StandardCharsets.US_ASCII).replace("?", "")
+      }
+      live_tweets.foreachRDD(
+        rdd => rdd.foreach(println(_))
+      )
+      live_tweets.foreachRDD(
+        rdd => rdd.pipe(command).collect().foreach(println(_))
+      )
+      ssc.checkpoint("src//main//Resources//checkpoint")
+      ssc.start()
+      ssc.awaitTermination()
 
     } catch {
       case exception1: NoSuchMethodError => println(exception1)
       case exception2: ClassNotFoundException => println(exception2)
       case exception3: InterruptedException => println(exception3)
-      case _ => println("Unknown Error occured!")
+      case _ => println("Unknown Error occurred!")
     }
   }
 
